c8ba182ff310d37ed182a2f6cdde162210708f9f
dagda1 <dagda1@scotalt.net>
2013-06-15 16:33:01 +0100

    update

--- packages/ember-autosuggest/lib/autosuggest.js
+++ packages/ember-autosuggest/lib/autosuggest.js
@@ -2,7 +2,7 @@ EmberAutosuggest.AutoSuggestView = Ember.View.extend({
   didInsertElement: function(){
     this._super.apply(this, arguments);
 
-    var input = this.$("<input class='autosuggest'/>");
+    var input = this.$("<input type='text' class='autosuggest'/>");
 
     this.$().append(input);
   }
