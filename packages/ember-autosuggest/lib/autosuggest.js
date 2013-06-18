2b1a5c6aeccbabe74f41cc291fa31c1779a6cf4a
dagda1 <dagda1@scotalt.net>
2013-06-18 09:28:48 +0100

    format string

--- packages/ember-autosuggest/lib/autosuggest.js
+++ packages/ember-autosuggest/lib/autosuggest.js
@@ -2,7 +2,12 @@ var precompileTemplate = Ember.Handlebars.compile;
 
 window.EmberAutosuggest.AutoSuggestView = Ember.View.extend({
   classNameBindings: [':autosuggest'],
-  defaultTemplate: precompileTemplate("<ul class='selections'><li>{{view view.autosuggest}}<\/li><\/ul><div class='results'><ul class='suggestions'><\/ul><\/div>"),
+  defaultTemplate: precompileTemplate("<ul class='selections'>" + 
+                                        "<li>{{view view.autosuggest}}<\/li>" + 
+                                      "<\/ul>"+ 
+                                      "<div class='results'>" + 
+                                         "<ul class='suggestions'><\/ul>" + 
+                                      "<\/div>"),
 
   autosuggest: Ember.TextField.extend({
     classNameBindings: [':autosuggest'],
